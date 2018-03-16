b8c2e781a3a17221bf67ba81b5f5db41081e0f62
Benedikt Mix <mixbened@gmail.com>
2018-03-16 11:59:26 -0700

    step 3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        
+        Picture:
+        <input />
+        Name:
+        <input />
+
+        <button>Add Friend</button>
       </div>
     );
   }
